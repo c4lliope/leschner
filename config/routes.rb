8b27dff07729544e7a4338394dd7a68933b444cb
knopfler81 <knopfler42@yahoo.fr>
2016-07-24 01:22:19 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  # match '/messages',     to: 'contacts#new',      via: 'get'
+  match '/messages',     to: 'contacts#new',      via: 'get'
   resources "contacts", only: [:new, :create]
+
+  match '/messages',     to: 'supports#new',      via: 'get'
   resources "supports", only: [:new, :create]
+
   root "home#index"
 
 end
