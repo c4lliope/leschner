71345b0e10e576f232795729b53a0a5741fefe9d
knopfler81 <knopfler42@yahoo.fr>
2016-07-23 23:52:15 +0200

    hide new contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # match '/messages',     to: 'contacts#new',      via: 'get'
   resources "contacts", only: [:new, :create]
-  resources "admin_contacts", only: [:new, :create]
+  # resources "admin_contacts", only: [:new, :create]
 
   root "home#index"
 
