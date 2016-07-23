6a5cdc7de37ea39427ab3d6a3fdaa94cf04ce78c
knopfler81 <knopfler42@yahoo.fr>
2016-07-24 01:38:41 +0200

    Trying to resolve support contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
-  match '/messages',     to: 'contacts#new',      via: 'get'
-  resources "contacts", only: [:new, :create]
 
-  match '/messages',     to: 'supports#new',      via: 'get'
+  resources "contacts", only: [:new, :create]
   resources "supports", only: [:new, :create]
 
   root "home#index"
