958dec4a16dbf30da511bb923e043be07bb613db
knopfler81 <knopfler42@yahoo.fr>
2016-07-23 22:55:46 +0200

    Setting the admin contact

---
+++ app/controllers/admin_contacts_controller.rb
@@ -0,0 +1,17 @@
+class AdminContactsController < ApplicationController
+  def new
+    @admin_contact = AdminContact.new
+  end
+
+  def create
+    @admin_contact = AdminContact.new(params[:admin_contact])
+    @admin_contact.request = request
+    if @admin_contact.deliver
+      flash.now[:error] = nil
+      flash.now[:notice]
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+  end
+end
