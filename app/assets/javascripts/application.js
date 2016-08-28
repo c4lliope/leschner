0a4e202b34e666eafa2a11a597f33e4f2a7a612a
knopfler81 <knopfler42@yahoo.fr>
2016-08-29 01:01:17 +0200

    changed image to an url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require masonry/jquery.masonry
-//= require masonry/jquery.event-drag
-//= require masonry/jquery.imagesloaded.min
-//= require masonry/jquery.infinitescroll.min
-//= require masonry/modernizr-transitions
 //= require turbolinks
-//= require bootstrap
 //= require_tree .
