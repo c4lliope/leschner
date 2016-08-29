1d686f9096364be94b5e3c7f4c5c5522fc830921
knopfler81 <knopfler42@yahoo.fr>
2016-08-29 10:50:34 +0200

    setting aws

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,5 @@
-# encoding: utf-8
-
 class ImageUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
    include CarrierWave::MiniMagick
 
    process :resize_to_fit =>[500, 500]
