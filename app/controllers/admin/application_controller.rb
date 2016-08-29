935d60d861d21db8f081b59aa23165909d297124
knopfler81 <knopfler42@yahoo.fr>
2016-08-29 15:34:54 +0200

    changed link

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,9 +1,3 @@
-# All Administrate controllers inherit from this `Admin::ApplicationController`,
-# making it the ideal place to put authentication logic or other
-# before_filters.
-#
-# If you want to add pagination or other controller-level concerns,
-# you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_filter :authenticate_admin
@@ -11,8 +5,6 @@ module Admin
 
       http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
 
-
-      # http_basic_authenticate_with name: ENV.fetch("ADMIN_NAME"), password: ENV.fetch("ADMIN_PASSWORD")
       #http_basic_authenticate_with name: "admin", password: "123456"
 
 
@@ -20,10 +12,6 @@ module Admin
       # TODO Add authentication logic here.
     end
 
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
+
   end
 end
