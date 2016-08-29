c116ff78df268e7889ef349cefd7a86a27e934d4
knopfler81 <knopfler42@yahoo.fr>
2016-08-29 14:43:32 +0200

    initializer changed

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,30 +1,30 @@
 if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.fog_credentials = {
-#       # Configuration for Amazon S3
-#       :provider              => 'AWS',
-#       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-#       :aws_secret_access_key => ENV['S3_SECRET_KEY']
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+
+    }
+    config.fog_directory     =  ENV['S3_BUCKET_NAME']
+  end
+end
+
+# CarrierWave.configure do |config|
+#   config.storage    = :aws
+#   config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+#   config.aws_acl    = 'public-read'
 #
-#     }
-#     config.fog_directory     =  ENV['S3_BUCKET']
-#   end
+#   # Optionally define an asset host for configurations that are fronted by a
+#   # content host, such as CloudFront.
+#   config.asset_host = 'http://leschnerguitars.herokuapp.com'
+#
+#
+#   config.aws_credentials = {
+#     access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+#     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+#   }
+#
 # end
-#
-CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
-  config.aws_acl    = 'public-read'
-
-  # Optionally define an asset host for configurations that are fronted by a
-  # content host, such as CloudFront.
-  config.asset_host = 'http://leschnerguitars.herokuapp.com'
-
-
-  config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-  }
-
-end
-end
+# end
