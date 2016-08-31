7a292911171c68d205e355c67e3babe9df073a73
knopfler81 <knopfler42@yahoo.fr>
2016-08-31 11:25:44 +0200

    set admin to dev and prod

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,11 +2,11 @@ module Admin
   class ApplicationController < Administrate::ApplicationController
     before_filter :authenticate_admin
 
-
-      http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
-
-      #http_basic_authenticate_with name: "admin", password: "123456"
-
+      if Rails.env.production?
+        http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
+      else
+        http_basic_authenticate_with name: "admin", password: "123456"
+      end
 
     def authenticate_admin
       # TODO Add authentication logic here.
