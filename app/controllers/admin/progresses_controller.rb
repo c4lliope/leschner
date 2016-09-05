fc19fc8055ba64bb2a198a889144ef65e7dd046a
knopfler81 <knopfler42@yahoo.fr>
2016-09-05 18:25:46 +0200

    error almost fixed

--- app/controllers/admin/progresses_controller.rb
+++ app/controllers/admin/progresses_controller.rb
@@ -1,12 +1,17 @@
 module Admin
+
   class ProgressesController < Admin::ApplicationController
+
     def create
        @progress = Progress.new(progress_params)
 
        respond_to do |format|
          if @progress.save
-           progress_params[:progress_attachments]['image'].each do |a|
-              @progress_attachment = @progress.progress_attachments.create!(:image => a)
+           unless params[:progress_attachments].nil?
+             params[:progress_attachments]['image'].each do |a|
+               @progress_attachment = @progress.progress_attachments.create!(:image => a)
+             end
+
            end
            format.html { redirect_to admin_progresses_path, notice: 'Progress was successfully created.' }
          else
@@ -16,8 +21,18 @@ module Admin
      end
 
      private
-       def progress_params
-          params.require(:progress).permit(:title, :content, :date, :main_image, progress_attachments_attributes: [:id, :progress_id, :image])
-       end
+
+     def progress_params
+        params.require(:progress).permit(:title, :content, :date, :main_image, progress_attachments_attributes: [:id, :progress_id, :image])
+     end
+
   end
+
 end
+
+
+# unless params[:progress_attachments].nil?
+#   params[:progress_attachments]['image'].each do |a|
+#     @progress_attachment = @progress.progress_attachments.create!(:image => a)
+#   end
+# end
